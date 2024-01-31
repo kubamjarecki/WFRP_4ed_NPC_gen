4b94d0aded8ee2568c3843b7c840af2a9d3b73a7
kubamjarecki <115503647+kubamjarecki@users.noreply.github.com>
2024-01-31 20:21:16 +0100

    Update Characteristics.py
    Dodałem informacjeo tym w jakim przedziale mają byc wpisywane rzuty dla użytkownika

--- classes/Characteristics.py
+++ classes/Characteristics.py
@@ -13,6 +13,7 @@ class Characteristics:
         while True:
             eager = input("Czy chcesz wpisać własne rzuty na cechy(t/n) ")
             if eager == "t" or eager == "T":
+                print("Wpisz cyfrę pomiędzy 2 a 20")
                 zasieg = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18",
                           "19", "20"]
 
